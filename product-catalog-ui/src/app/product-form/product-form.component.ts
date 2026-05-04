9984299fb0667542c6adf54c127f2f36dd315496
WASOWSKI_DELL\wasol <wasol1980@gmail.com>
2026-05-04 13:46:29 +0200

    Add front validation

--- product-catalog-ui/src/app/product-form/product-form.component.ts
+++ product-catalog-ui/src/app/product-form/product-form.component.ts
@@ -1,5 +1,6 @@
 import { Product } from "../models/product";
 import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { ProductService } from "../product.service";
 import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,7 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
 })
 export class ProductFormComponent {
   product: Product = {
@@ -21,11 +22,12 @@ export class ProductFormComponent {
 
   constructor(private productService: ProductService) { }
 
-  submit() {
+  submit(form: any) {
     this.productService.addProduct(this.product)
       .subscribe(() => {
         alert('Produkt został dodany');
         this.product = { kod: '', nazwa: '', cena: 0 };
+        form.resetForm();
     });
   }
 }
